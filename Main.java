3d6e6b682e506f61a703badffb005e43ed0917fd
Maria Ma <maria.shurui.ma@gmail.com>
2023-10-01 00:20:59 -0400

    added java file

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         Request request = new Request.Builder()
-                .url(String.format("https://api.spoonacular.com/recipes/complexSearch?apiKey=%s&query=%s&number=%s", "28252b1aac05401cbd3c80f760617063", "lobster", "1"))
+                .url(String.format("https://api.spoonacular.com/recipes/complexSearch?apiKey=%s&query=%s&number=%s", API_TOKEN, "lobster", "1"))
                 .build();
         try {
             Response response = client.newCall(request).execute();
