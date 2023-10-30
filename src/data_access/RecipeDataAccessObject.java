98a49d29e255ee96ad3e5b25fbd4d3304ac5a62f
Michelle Wang <xxw0922@gmail.com>
2023-10-30 18:27:58 -0400

    modified BrowseDataAccessInterface; initialize RecipeDataAccessObject and let it implement BrowseDataAccessInterface

--- src/data_access/RecipeDataAccessObject.java
+++ src/data_access/RecipeDataAccessObject.java
@@ -1,13 +1,24 @@
 package data_access;
 
+import entity.Recipe;
+import entity.RecipeInfo;
 import use_case.browse.BrowseDataAccessInterface;
 import use_case.browse.BrowseInputData;
 import use_case.recommend.RecommendDataAccessInterface;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RecipeDataAccessObject implements BrowseDataAccessInterface, RecommendDataAccessInterface {
+    private final Map<String, Recipe> savedRecipes = new HashMap<>();
 
-    public void execute(BrowseInputData browseInputData) {
+    @Override
+    public ArrayList<Recipe> browse(BrowseInputData browseInputData) {
+        return null;
+    }
 
-    };
-
+    public RecipeInfo getRecipeInfo(int recipeID){
+        return null;
+    }
 }
--- src/use_case/browse/BrowseDataAccessInterface.java
+++ src/use_case/browse/BrowseDataAccessInterface.java
@@ -1,7 +1,11 @@
 package use_case.browse;
 
+import entity.Recipe;
+
+import java.util.ArrayList;
+
 public interface BrowseDataAccessInterface {
 
-    void execute(BrowseInputData browseInputData);
+    ArrayList<Recipe> browse(BrowseInputData browseInputData);
 
 }
