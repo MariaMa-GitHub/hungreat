1ad67643d02894ac7cda324faae1dfa0d14b6e58
Michelle Wang <xxw0922@gmail.com>
2023-11-10 01:13:45 -0500

    added potential attributes and constructor for BrowseFilter

--- src/entity/BrowseFilter.java
+++ src/entity/BrowseFilter.java
@@ -1,4 +1,18 @@
 package entity;
 
+import java.util.Map;
+
 public class BrowseFilter extends Filter {
+    private final String query;
+//    private final String ingredients;
+//    private final String cuisine;
+//    private final String excludeCuisine;  //TODO: if keep, add to constructor and add getters
+
+    public BrowseFilter(String diet, String intolerance, String excludeIngredients, Map<String, Float> nutrients,
+    String query) {
+        super(diet, intolerance, excludeIngredients, nutrients);
+        this.query = query;
+    }
+
+
 }
