69f26ec8132cb47522bfd2518716f7f725ce6f24
Michelle Wang <xxw0922@gmail.com>
2023-11-10 00:58:04 -0500

    added getters for potential attributes in Filter.

--- src/entity/Filter.java
+++ src/entity/Filter.java
@@ -4,11 +4,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Filter {
-    private String diet;
-    private String intolerance;
-    private String excludeIngredients;
-    private Map<String, Float> nutrients;
-//    private String sort;
+    private final String diet;
+    private final String intolerance;
+    private final String excludeIngredients;
+    private final Map<String, Float> nutrients;   //TODO:why nutrients has it own data type?
+//    private String sort
 //    private String sortDirection;
-//    private boolean instructionsRequired;
+//    private boolean instructionsRequired;     //Todo: if keep, remember to add to constructor & add getters
+
+    public Filter(String diet, String intolerance, String excludeIngredients, Map<String, Float> nutrients) {
+        this.diet = diet;
+        this.intolerance = intolerance;
+        this.excludeIngredients = excludeIngredients;
+        this.nutrients = nutrients;
+    }
+
+    public String getDiet() {
+        return diet;
+    }
+
+    public String getIntolerance() {
+        return intolerance;
+    }
+
+    public String getExcludeIngredients() {
+        return excludeIngredients;
+    }
+
+    public Map<String, Float> getNutrients() {
+        return nutrients;
+    }
 }
