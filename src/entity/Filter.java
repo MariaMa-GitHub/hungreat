2743ba1394bc52aa74c37d12e838e632e6ac657f
Michelle Wang <xxw0922@gmail.com>
2023-11-10 00:51:38 -0500

    added potential attributes for the base filter entity class.

--- src/entity/Filter.java
+++ src/entity/Filter.java
@@ -1,4 +1,14 @@
 package entity;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Filter {
+    private String diet;
+    private String intolerance;
+    private String excludeIngredients;
+    private Map<String, Float> nutrients;
+//    private String sort;
+//    private String sortDirection;
+//    private boolean instructionsRequired;
 }
