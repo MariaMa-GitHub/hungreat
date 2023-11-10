99164db66cf6d2a7baf487b0344a1bf31080b9ef
Michelle Wang <xxw0922@gmail.com>
2023-11-10 01:07:22 -0500

    added potential attributes and constructor for RecommendFilter

--- src/entity/RecommendFilter.java
+++ src/entity/RecommendFilter.java
@@ -1,4 +1,20 @@
 package entity;
 
+import java.util.Map;
+
 public class RecommendFilter extends Filter{
+    private final String ingredients;
+    private final String cuisine;
+    private final String excludeCuisine;
+    private final String type;
+
+    public RecommendFilter(String diet, String intolerance, String ingredients, String excludeIngredients,
+                           String cuisine, String excludeCuisine, String type, Map<String, Float> nutrients) {
+        super(diet, intolerance, excludeIngredients, nutrients);
+        this.ingredients = ingredients;
+        this.cuisine = cuisine;
+        this.excludeCuisine = excludeCuisine;
+        this.type = type;
+    }
+
 }
