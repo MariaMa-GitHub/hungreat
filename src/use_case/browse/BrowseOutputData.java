2f56eaea6e05fc60babed5ce88833832aa4ed222
Chloe <ysj15265673506@163.com>
2023-11-13 18:50:11 -0500

    finished BrowseOutputData

--- src/use_case/browse/BrowseOutputData.java
+++ src/use_case/browse/BrowseOutputData.java
@@ -1,4 +1,16 @@
 package use_case.browse;
 
+import entity.Recipe;
+
+import java.util.ArrayList;
+
 public class BrowseOutputData {
+    final ArrayList<Recipe> recipes;
+    public BrowseOutputData(ArrayList<Recipe> recipes){
+        this.recipes = recipes;
+    }
+    public ArrayList<Recipe> getRecipes(){
+        return recipes;
+    }
+
 }
