165213b0623d29dcd59b36f64d8457d98f4e8c96
Chloe <ysj15265673506@163.com>
2023-11-13 19:10:33 -0500

    updated excute in BrowseInteractor

--- src/use_case/browse/BrowseInteractor.java
+++ src/use_case/browse/BrowseInteractor.java
@@ -1,8 +1,12 @@
 package use_case.browse;
 
+import entity.BrowseFilter;
+import entity.Filter;
+import entity.Recipe;
 import use_case.recommend.RecommendInputData;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class BrowseInteractor implements BrowseInputBoundary {
 
@@ -17,6 +21,17 @@ public class BrowseInteractor implements BrowseInputBoundary {
 
     @Override
     public void execute(BrowseInputData browseInputData) {
+        String diet = browseInputData.getDiet();
+        String intolerance = browseInputData.getIntolerance();
+        String excludeIngredients = browseInputData.getExcludeIngredients();
+        Map<String, Float> nutrients = browseInputData.getNutrients();
+        String query = browseInputData.getQuery();
+
+        BrowseFilter browseFilter = new BrowseFilter(diet, intolerance, excludeIngredients, nutrients, query);
+        BrowseOutputData browseOutputData = new BrowseOutputData(this.dataAccessObject.browse(browseFilter));
+        //if not arrylist then handle fail veiw.
+        browseOutputData.getRecipes();
+
 
     }
 }
