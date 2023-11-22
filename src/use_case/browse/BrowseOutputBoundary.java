870d016c564eed5623ff1c62afb6aad9c88143ff
Chloe <ysj15265673506@163.com>
2023-11-21 21:19:56 -0500

    Finished implement Browse use case

--- src/use_case/browse/BrowseOutputBoundary.java
+++ src/use_case/browse/BrowseOutputBoundary.java
@@ -3,7 +3,7 @@ package use_case.browse;
 import java.util.Map;
 
 public interface BrowseOutputBoundary {
-    void prepareSuccessView(Map<Integer, String> id_title);
+    void prepareSuccessView(BrowseOutputData id_title);
 
     void prepareFailView(String error);
 
