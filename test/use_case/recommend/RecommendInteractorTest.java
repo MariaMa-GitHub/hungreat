f618fd09c72d7337cc7341326054ebe5ec3a3d64
Maria Ma <maria.shurui.ma@gmail.com>
2023-11-21 21:24:46 -0500

    Started writing test cases for recommend

---
+++ test/use_case/recommend/RecommendInteractorTest.java
@@ -0,0 +1,18 @@
+package use_case.recommend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecommendInteractorTest {
+
+    private RecommendInteractor recommendInteractor;
+    private
+
+    @Before
+    public void init() {
+        recommendInteractor = new RecommendInteractor();
+        days = week.iterator();
+    }
+
+}
