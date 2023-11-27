5ceb83a6d02bdce5e296867023d470e16f9d535c
Henry-wxq <henry.wxq.1118@gmail.com>
2023-11-27 15:08:27 -0500

    Updated the BrowsePresenter.java

--- src/interface_adapter/browse/BrowsePresenter.java
+++ src/interface_adapter/browse/BrowsePresenter.java
@@ -1,18 +1,25 @@
 package interface_adapter.browse;
 
+import java.util.Map;
 import use_case.browse.BrowseOutputBoundary;
 import use_case.browse.BrowseOutputData;
 
-import java.util.Map;
-
 public class BrowsePresenter implements BrowseOutputBoundary {
-    @Override
-    public void prepareSuccessView(BrowseOutputData idTitle) {
 
+    @Override
+    public void prepareSuccessView(BrowseOutputData idTitleData) {
+        // Process BrowseOutputData and display success view accordingly
+        Map<Integer, String> idTitleMap = idTitleData.getID_title();
+        for (Map.Entry<Integer, String> entry : idTitleMap.entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+        System.out.println("Successful view prepared");
     }
 
     @Override
     public void prepareFailView(String error) {
-
+        // Display error view with the provided error message
+        System.err.println("Error occurred: " + error);
     }
 }
+
