fe21f6c2d2d621c96a109b7b5e4b2ca61964e200
Henry-wxq <henry.wxq.1118@gmail.com>
2023-11-27 17:19:50 -0500

    Updated the BrowseController.java

--- src/interface_adapter/browse/BrowseController.java
+++ src/interface_adapter/browse/BrowseController.java
@@ -1,21 +1,5 @@
 package interface_adapter.browse;
 
-//import interface_adapter.SearchController;
-//import use_case.browse.BrowseInputBoundary;
-//
-//public class BrowseController implements SearchController {
-//
-//    final BrowseInputBoundary browseInteractor;
-//
-//    public BrowseController(BrowseInputBoundary browseInteractor) {
-//        this.browseInteractor = browseInteractor;
-//    }
-//
-//    public void execute() {
-////        System.out.println("YESSSSSSSSS");
-//    }
-//}
-
 import use_case.browse.BrowseInputBoundary;
 import use_case.browse.BrowseInputData;
 
