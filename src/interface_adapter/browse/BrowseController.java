d77f2454dd534be5332d17d59b120f38a4878715
Henry-wxq <henry.wxq.1118@gmail.com>
2023-11-27 15:14:36 -0500

    Updated the BrowseController.java

--- src/interface_adapter/browse/BrowseController.java
+++ src/interface_adapter/browse/BrowseController.java
@@ -1,18 +1,35 @@
 package interface_adapter.browse;
 
-import interface_adapter.SearchController;
+//import interface_adapter.SearchController;
+//import use_case.browse.BrowseInputBoundary;
+//
+//public class BrowseController implements SearchController {
+//
+//    final BrowseInputBoundary browseInteractor;
+//
+//    public BrowseController(BrowseInputBoundary browseInteractor) {
+//        this.browseInteractor = browseInteractor;
+//    }
+//
+//    public void execute() {
+////        System.out.println("YESSSSSSSSS");
+//    }
+//}
+
 import use_case.browse.BrowseInputBoundary;
+import use_case.browse.BrowseInputData;
 
-public class BrowseController implements SearchController {
+public class BrowseController implements BrowseInputBoundary {
+    private final use_case.browse.BrowseInputBoundary browseInteractor;
 
-    final BrowseInputBoundary browseInteractor;
-
-    public BrowseController(BrowseInputBoundary browseInteractor) {
+    public BrowseController(use_case.browse.BrowseInputBoundary browseInteractor) {
         this.browseInteractor = browseInteractor;
     }
 
-    public void execute() {
-//        System.out.println("YESSSSSSSSS");
+    @Override
+    public void execute(BrowseInputData browseInputData) {
+        browseInteractor.execute(browseInputData);
     }
-
 }
+
+
