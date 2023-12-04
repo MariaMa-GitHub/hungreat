a027ab4a13e503e731adfe2337018630da316291
Henry-wxq <henry.wxq.1118@gmail.com>
2023-12-04 17:14:11 -0500

    Updated SaveViewModel.java

--- src/interface_adapter/SaveViewModel.java
+++ src/interface_adapter/SaveViewModel.java
@@ -1,10 +1,12 @@
 package interface_adapter;
 
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.util.Map;
 
 public class SaveViewModel {
-    private String title;
     private Map<Integer, String> savedRecipes;
+    private String error;
 
     public SaveViewModel() {
     }
@@ -17,7 +19,31 @@ public class SaveViewModel {
         this.savedRecipes = savedRecipes;
     }
 
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
     public void add(Integer recipeID, String title) {
         savedRecipes.put(recipeID, title);
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void successFirePropertyChanged() {
+        // TODO: Check whether null is appropriate for the first argument
+        support.firePropertyChange("savedRecipes", null, this.savedRecipes);
+    }
+
+    public void failFirePropertyChanged() {
+        // TODO: Check whether null is appropriate for the first argument
+        support.firePropertyChange("error", null, this.error);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
     }
 }
