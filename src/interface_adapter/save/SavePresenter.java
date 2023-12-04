80a3efcbbb5c4e0ae527aa2795b80954a5ed9e6a
Henry-wxq <henry.wxq.1118@gmail.com>
2023-12-04 16:34:26 -0500

    Updated SavePresenter.java

--- src/interface_adapter/save/SavePresenter.java
+++ src/interface_adapter/save/SavePresenter.java
@@ -1,4 +1,23 @@
 package interface_adapter.save;
 
-public class SavePresenter {
+import interface_adapter.SaveViewModel;
+import use_case.save.SaveOutputBoundary;
+import use_case.save.SaveOutputData;
+
+public class SavePresenter implements SaveOutputBoundary {
+    private final SaveViewModel saveViewModel;
+
+    public SavePresenter(SaveViewModel saveViewModel) {
+        this.saveViewModel = saveViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(SaveOutputData Title) {
+        System.out.println("Recipe saved successfully!");
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        System.err.println("Error occurred: " + error);
+    }
 }
