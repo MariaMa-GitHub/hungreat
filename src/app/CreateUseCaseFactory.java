9342e90e700d7960f6bda639dd5175a8b95860cf
Michelle Wang <xxw0922@gmail.com>
2023-12-04 15:57:02 -0500

    Implemented the CreateUseCaseFactory. Deleted the CreateState and CreateViewModel as create will use the saveViewModel.

--- src/app/CreateUseCaseFactory.java
+++ src/app/CreateUseCaseFactory.java
@@ -1,4 +1,42 @@
 package app;
 
+import entity.RecipeFactory;
+import entity.RecipeInfoFactory;
+import interface_adapter.create.CreateController;
+import interface_adapter.create.CreatePresenter;
+import use_case.create.CreateDataAccessInterface;
+import use_case.create.CreateInputBoundary;
+import use_case.create.CreateInteractor;
+import use_case.create.CreateOutputBoundary;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public class CreateUseCaseFactory {
+    private CreateUseCaseFactory() {}
+
+    public static CreateController create(CreateDataAccessInterface dataAccessObject, SaveViewModel saveViewModel) {
+
+        try {
+            CreateController createController = createCreateUseCase(dataAccessObject, saveViewModel);
+            return createController;
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Unable to save the recipe.");
+        }
+
+        return null;
+    }
+
+    private static CreateController createCreateUseCase(CreateDataAccessInterface dataAccessObject, SaveViewModel saveViewModel) throws IOException{
+
+        CreateOutputBoundary createOutputBoundary = new CreatePresenter(saveViewModel);
+
+        RecipeFactory recipeFactory = new RecipeFactory();
+        RecipeInfoFactory recipeInfoFactory = new RecipeInfoFactory();
+
+        CreateInputBoundary createInteractor = new CreateInteractor(dataAccessObject, createOutputBoundary, recipeFactory, recipeInfoFactory);
+
+        return new CreateController(createInteractor);
+    }
+
 }
