f99d47159a7a2e76811b05b878b74339ba8ba604
Henry-wxq <henry.wxq.1118@gmail.com>
2023-12-04 17:14:03 -0500

    Updated SaveView.java

--- src/view/SaveView.java
+++ src/view/SaveView.java
@@ -1,6 +1,14 @@
 package view;
 
+import javax.swing.*;
+
 public class SaveView {
     // TODO: a popup window mention whether save successfully
+    public void showSaveSuccess() {
+        JOptionPane.showMessageDialog(null, "Successfully saved!");
+    }
 
+    public void showSaveFail(String error) {
+        JOptionPane.showMessageDialog(null, error);
+    }
 }
