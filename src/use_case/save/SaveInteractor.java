ac1dce72bc45a32fe021a5925dd7b39bd97e4b27
Henry-wxq <henry.wxq.1118@gmail.com>
2023-12-04 16:33:46 -0500

    Updated SaveInteractor.java

--- src/use_case/save/SaveInteractor.java
+++ src/use_case/save/SaveInteractor.java
@@ -1,6 +1,40 @@
 package use_case.save;
 
-public class SaveInteractor {
+import entity.Recipe;
+import use_case.TemporaryRecipeDataAccessInterface;
+
+import java.util.ArrayList;
+
+public class SaveInteractor implements SaveInputBoundary {
+    final SaveOutputBoundary saveOutputBoundary;
+    final TemporaryRecipeDataAccessInterface temporaryRecipeDataAccessInterface;
+    final SaveDataAccessInterface savedRecipeDataAccessInterface;
+
+    public SaveInteractor(SaveOutputBoundary saveOutputBoundary, TemporaryRecipeDataAccessInterface temporaryRecipeDataAccessInterface, SaveDataAccessInterface savedRecipeDataAccessInterface) {
+        this.saveOutputBoundary = saveOutputBoundary;
+        this.temporaryRecipeDataAccessInterface = temporaryRecipeDataAccessInterface;
+        this.savedRecipeDataAccessInterface = savedRecipeDataAccessInterface;
+    }
+
     // TODO: Call the storeRecipe method from TemporaryRecipeDataAccessObject
     // TODO: Call the save method from SavedRecipeDataAccessObject
+    @Override
+    public void execute(SaveInputData saveInputData) {
+        Integer recipeID = saveInputData.getRecipeID();
+        Recipe saveRecipeTitle = temporaryRecipeDataAccessInterface.getFromID(recipeID);
+        try{
+            savedRecipeDataAccessInterface.save(saveRecipeTitle);
+            SaveOutputData saveOutputData = new SaveOutputData(recipeID, saveRecipeTitle.getTitle());
+            saveOutputBoundary.prepareSuccessView(saveOutputData);
+        }
+        catch (Exception e){
+            String errorMessage = e.getMessage();
+            saveOutputBoundary.prepareFailView(errorMessage);
+        }
+    }
+
+    @Override
+    public void execute() {
+
+    }
 }
