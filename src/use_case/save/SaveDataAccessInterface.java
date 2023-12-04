70c81105947b1c053c823111b1c573402e3723cf
Henry-wxq <henry.wxq.1118@gmail.com>
2023-12-04 16:32:39 -0500

    Updated SaveDataAccessInterface.java

--- src/use_case/save/SaveDataAccessInterface.java
+++ src/use_case/save/SaveDataAccessInterface.java
@@ -1,4 +1,12 @@
 package use_case.save;
 
+import entity.Recipe;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public interface SaveDataAccessInterface {
+    void save(Recipe recipe) throws IOException, ClassNotFoundException;
+
+    void read();
 }
